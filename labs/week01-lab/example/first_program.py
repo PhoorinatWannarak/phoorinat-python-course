c09d12bac7306abe3105c97a7853c66477c76ea2
PhoorinatWannarak <phoorinat.w@ku.th>
2026-07-03 11:46:24 +0700

    my_first_commit

--- labs/week01-lab/example/first_program.py
+++ labs/week01-lab/example/first_program.py
@@ -4,7 +4,7 @@ print("Hello, World!")
 
 # Exercise 2: Custom Print Statement
 # Write a print statement that displays your name, e.g., "My name is Your Name
-print("My name is Your Name")
+print("My name is Phoorinat Wannarak")
 
 # Exercise 3: Multiple Print Statements
 # Write three print statements that display:
@@ -12,7 +12,9 @@ print("My name is Your Name")
 # Line 2: Your age
 # Line 3: Your favorite hobbyๅ
 # Write your code here:
-
+print("phoorinat love python")
+print("19")
+print("eat")
 
 # Exercise 4: Print with Different Content
 # Use print() to display the following (one per line):
@@ -21,8 +23,9 @@ print("My name is Your Name")
 # - A sentence with spaces
 
 # Write your code here:
-
-
+print("676767676767")
+print("immortal")
+print("kasetsart")
 
 # Exercise 5: Understanding Print Behavior
 # What happens when you run these? Try to predict before running:
@@ -33,11 +36,11 @@ print("Third", "line", "with", "multiple", "arguments")
 # Exercise 6: Comments Practice
 # Add comments above each print statement explaining what it does
 
-# 
+# welcome tp python programming
 print("Welcome to Python programming!")
 
-# 
+# python
 print("Python is fun!")
 
-# 
+# teach me more
 print("I am learning to code!")
